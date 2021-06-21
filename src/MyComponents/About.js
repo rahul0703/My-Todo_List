6c3c7022183f3792cd0ab6fa961c8484d73e026b
rahul007 <rahulsureka516@gmail.com>
2021-06-21 18:07:00 +0530

    my app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className="container">
+            <p>This a Todo List Application made using React made by Rahul Sureka. </p>
+        </div>
+    )
+}
